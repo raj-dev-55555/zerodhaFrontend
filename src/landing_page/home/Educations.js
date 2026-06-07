a8291424529b92908313711e0949719927cc65bf
vinay <abc@gmail.com>
2026-06-07 16:06:57 +0530

    update navbar

--- src/landing_page/home/Educations.js
+++ src/landing_page/home/Educations.js
@@ -2,26 +2,21 @@ import React from "react";
 
 function Educations() {
     return (
-
         <div className="container mt-5">
             <div className="row">
-                <div className="col-6" >
-                    <img src="media\images\education.svg"></img>
+                <div className="col-12 col-md-6 text-center">
+                    <img src="media/images/education.svg" className="img-fluid"></img>
                 </div>
-                <div className="col-6 mt-5" >
+                <div className="col-12 col-md-6 mt-5">
                     <h2 className="mb-3 text-muted">Free and open market educations</h2>
                     <p>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
-                    <a href="" className="mx-5" style={{ textDecoration: "none" }}>Varsity <i class="fa-solid fa-arrow-right"></i></a>
+                    <a href="" className="mx-5" style={{ textDecoration: "none" }}>Varsity <i className="fa-solid fa-arrow-right"></i></a>
 
                     <p className="mt-5">TradingQ&A, the most active trading and investment community in India for all your market related queries.</p>
-                    <a href="" className="mx-5" style={{ textDecoration: "none" }}>TradingQ&A <i class="fa-solid fa-arrow-right"></i></a>
-
+                    <a href="" className="mx-5" style={{ textDecoration: "none" }}>TradingQ&A <i className="fa-solid fa-arrow-right"></i></a>
                 </div>
-
             </div>
         </div>
-
-
     )
 }
 
--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -2,34 +2,28 @@ import React from "react";
 
 function Pricing(){
     return(
-      
-   <div className="container">
-     <div className="row">
-           <div className="col-4" >
-                   <h1 className="mb-3">Unbeatable pricing</h1>
-                     <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
-                    <a href="" className="mx-5" style={{textDecoration:"none"}}>See Pricings<i class="fa-solid fa-arrow-right"></i></a>
-
-                             </div>
-                             <div className="col-2" ></div>
-                             <div className="col-6" >
-                                <div className="row text-center">
-                             <div className="col p-3 border" >
-                                <h1 className="mb-3">0</h1>
-                                <p>  Free account<br></br> opening </p>
-                             </div>
-                             <div className="col p-3 border" >
-                                <h1 className="mb-3">20</h1>
-                                <p>   Intraday and <br></br>F&O </p>
-
-                             </div>
-                                       
-                                </div>
-                             </div>
-             </div>
-
-          </div>
-        
+        <div className="container">
+            <div className="row align-items-center">
+                <div className="col-12 col-md-4 mb-4">
+                    <h1 className="mb-3">Unbeatable pricing</h1>
+                    <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                    <a href="" className="mx-5" style={{textDecoration:"none"}}>See Pricings <i className="fa-solid fa-arrow-right"></i></a>
+                </div>
+                <div className="col-md-2 d-none d-md-block"></div>
+                <div className="col-12 col-md-6 mb-4">
+                    <div className="row text-center">
+                        <div className="col-6 p-3 border">
+                            <h1 className="mb-3">0</h1>
+                            <p>Free account<br/>opening</p>
+                        </div>
+                        <div className="col-6 p-3 border">
+                            <h1 className="mb-3">20</h1>
+                            <p>Intraday and<br/>F&O</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
